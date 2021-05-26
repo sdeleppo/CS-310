dca7aed77f0098e72ec2b3d696f4c8b8e0dbc76c
Philip Enkema <p.enkema@snhu.edu>
2021-05-26 20:46:00 +0000

    added songs

--- src/snhu/jukebox/playlist/tests/StudentTest.java
+++ src/snhu/jukebox/playlist/tests/StudentTest.java
@@ -33,7 +33,7 @@ public class StudentTest {
 	//Module 5 - Add your unit test case here to check for your name after you have added it to the StudentList
 	
 	@Test
-	public void testGetStudentNameList3() {
+	public void testGetPhilipEnkemaList3() {
 		List<String> studentNames = new ArrayList<String>();							//create variable for student list of names
 		StudentList studentList = new StudentList();									//instantiate the StudentList object so we can access it's methods and properties
 		studentNames = studentList.getStudentsNames();									//populate the studentNames list with the actual values in the StudentsList object
@@ -59,4 +59,10 @@ public class StudentTest {
 	
 	//Module 6 - Add your unit test case here to check for your profile after you have added it to the StudentList
 	
+	@Test
+	public void testGetPhilipEnkemaProfile() {
+		PhilipEnkema_Playlist PhilipEnkema_Playlist = new PhilipEnkema_Playlist();
+		Student PhilipEnkema = new Student("Philip Enkema", PhilipEnkema_Playlist.StudentPlaylist());
+		assertEquals("Philip Enkema", PhilipEnkema.getName());
+	}
 }
