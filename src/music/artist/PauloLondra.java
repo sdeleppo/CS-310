5c728ca8533f9c565cde07129489fb1834e5fe9a
Tyson Smith <tysonsmiths@gmail.com>
2021-06-03 23:20:59 -0600

    Fixed whitespacing

--- src/music/artist/PauloLondra.java
+++ src/music/artist/PauloLondra.java
@@ -17,14 +17,14 @@ public class PauloLondra {
     public ArrayList<Song> getPauloLondraSongs() {
     	
     	 albumTracks = new ArrayList<Song>();
-    	 Song track1 = new Song("Forever Alone", ARTIST_NAME);      
-         Song track2 = new Song("Chica Paranormal", ARTIST_NAME);    
-         Song track3 = new Song("Solo Pienso En Ti", ARTIST_NAME);     
-         Song track4 = new Song("Querido Amigo", ARTIST_NAME);      
-         this.albumTracks.add(track1);                   
-         this.albumTracks.add(track2);                               
-         this.albumTracks.add(track3);
-         this.albumTracks.add(track4); 
-         return albumTracks;                                            
+    	 Song track1 = new Song("Forever Alone", ARTIST_NAME);
+    	 Song track2 = new Song("Chica Paranormal", ARTIST_NAME);
+    	 Song track3 = new Song("Solo Pienso En Ti", ARTIST_NAME);
+    	 Song track4 = new Song("Querido Amigo", ARTIST_NAME);
+    	 this.albumTracks.add(track1);
+    	 this.albumTracks.add(track2);
+    	 this.albumTracks.add(track3);
+    	 this.albumTracks.add(track4);
+    	 return albumTracks;                            
     }
 }
