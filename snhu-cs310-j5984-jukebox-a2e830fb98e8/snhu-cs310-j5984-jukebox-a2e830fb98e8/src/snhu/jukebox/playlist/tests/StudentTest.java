2d112d9709a6a50257542b6e3077970e4538d0a2
Kaitlyn French <kaitlyn.french@snhu.edu>
2021-06-04 15:09:46 -0500

    Added test for KaitlynFrenchList5 to StudentTest.java

--- snhu-cs310-j5984-jukebox-a2e830fb98e8/snhu-cs310-j5984-jukebox-a2e830fb98e8/src/snhu/jukebox/playlist/tests/StudentTest.java
+++ snhu-cs310-j5984-jukebox-a2e830fb98e8/snhu-cs310-j5984-jukebox-a2e830fb98e8/src/snhu/jukebox/playlist/tests/StudentTest.java
@@ -54,6 +54,14 @@ public class StudentTest {
 		studentNames = studentList.getStudentsNames();									//populate the studentNames list with the actual values in the StudentsList object
 		assertEquals("Tyson Smith", studentNames.get(4));							//test case for pass/fail. We expect the first name to be TestStudent1Name. Remember arrays start their count at 0 not 1.
 	}
+	
+	@Test
+	public void testGetKaitlynFrenchList5() {
+		List<String> studentNames = new ArrayList<String>();							//create variable for student list of names
+		StudentList studentList = new StudentList();									//instantiate the StudentList object so we can access it's methods and properties
+		studentNames = studentList.getStudentsNames();									//populate the studentNames list with the actual values in the StudentsList object
+		assertEquals("Kaitlyn French", studentNames.get(2));				     			//test case to see if the third value contains the name we expect
+	}
 		
 	//Module 6 Test Case Area
 	//Test each student profile to ensure it can be retrieved and accessed
