4355dbf101f7484d7f818386c4d18a46c8d73416
1630911_snhu <1630911_snhu@NV-SNHU21-RDS1.apporto.com>
2021-06-08 12:20:09 -0400

    Added song to playlist per mod 6 instructions

--- src/snhu/student/playlists/SyndaSimmers_Playlist.java
+++ src/snhu/student/playlists/SyndaSimmers_Playlist.java
@@ -8,33 +8,30 @@ import java.util.LinkedList;
 
 public class SyndaSimmers_Playlist {
 	/* added for module 6 requirements
-	 * songs from Helb Alpert, Burt Bacharach, and Imagine Dragons
+	 * songs from StillWoozy and Gorillaz 
 	 */
     
 	public LinkedList<PlayableSong> StudentPlaylist(){
 	
 	LinkedList<PlayableSong> playlist = new LinkedList<PlayableSong>();
 	
-	ArrayList<Song> bacharachTracks = new ArrayList<Song>();
-	BurtBacharach burtBacharach = new BurtBacharach();
-	bacharachTracks = burtBacharach.getBurtBacharachSongs();
+	ArrayList<Song> StillWoozyTracks = new ArrayList<Song>();
+	StillWoozy stillWoozy = new StillWoozy();
+	StillWoozyTracks = stillWoozy.getStillWoozySongs();
 	
-	playlist.add(bacharachTracks.get(0));
-	playlist.add(bacharachTracks.get(1));
-	playlist.add(bacharachTracks.get(2));	
+	playlist.add(StillWoozyTracks.get(0));
+	playlist.add(StillWoozyTracks.get(1));
+	playlist.add(StillWoozyTracks.get(2));	
 
-	ArrayList<Song> herbAlpertTracks = new ArrayList<Song>();
-	HerbAlpert herbAlpert = new HerbAlpert();
-	herbAlpertTracks = herbAlpert.getHerbAlpertSongs();
 	
-	playlist.add(herbAlpertTracks.get(0));
-	playlist.add(herbAlpertTracks.get(1));
+	ArrayList<Song> GorillazTracks = new ArrayList<Song>();
+	Gorillaz gorillaz = new Gorillaz();
+	GorillazTracks = gorillaz.getGorillazSongs();
 	
-	ArrayList<Song> imagineDragonsTracks = new ArrayList<Song>();
-	ImagineDragons imagineDragonsBand = new ImagineDragons();
-    imagineDragonsTracks = imagineDragonsBand.getImagineDragonsSongs();
+	playlist.add(GorillazTracks.get(0));
+	playlist.add(GorillazTracks.get(1));
+	playlist.add(GorillazTracks.get(2));	
 	
-	playlist.add(imagineDragonsTracks.get(0));
 	
     return playlist;
 	}
