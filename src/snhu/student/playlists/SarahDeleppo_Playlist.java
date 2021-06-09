9f3643fe489004c8087c224bb5297a8e8e82e9af
Sarah Deleppo <sarah.deleppo@snhu.edu>
2021-06-09 13:22:29 -0400

    Populated SarahDeleppo_Playlist with songs from peer's class

--- src/snhu/student/playlists/SarahDeleppo_Playlist.java
+++ src/snhu/student/playlists/SarahDeleppo_Playlist.java
@@ -9,26 +9,34 @@ import java.util.LinkedList;
 public class SarahDeleppo_Playlist {
     
 	public LinkedList<PlayableSong> StudentPlaylist(){
-	//create linked list for personal playlist
+	//create list for personal playlist
 	LinkedList<PlayableSong> playlist = new LinkedList<PlayableSong>();
 	
-	//create arraylist to access tracks inside Gorillaz class, create instance of Gorillaz class
+	//instantiate class Gorillaz, create list for tracks, populate list with tracks
+	Gorillaz gorillazBand = new Gorillaz();
 	ArrayList<Song> gorillazTracks = new ArrayList<Song>();
-    Gorillaz gorillazBand = new Gorillaz();
-	//populate arraylist with tracks from Gorillaz class
     gorillazTracks = gorillazBand.getGorillazSongs();
 	//add those tracks to playlist
 	playlist.add(gorillazTracks.get(0));
 	playlist.add(gorillazTracks.get(1));
 	playlist.add(gorillazTracks.get(2));
 	
-	//instantiate class, create list for tracks, populate list with tracks
+	//instantiate class Phantogram, create list for tracks, populate list with tracks
     Phantogram phantogramBand = new Phantogram();
 	ArrayList<Song> phantogramTracks = new ArrayList<Song>();
     phantogramTracks = phantogramBand.getPhantogramSongs();
 	//add tracks to playlist
 	playlist.add(phantogramTracks.get(0));
 	playlist.add(phantogramTracks.get(1));
+	
+	//instantiate peer's class Nirvana, create list for tracks, populate list with tracks
+	Nirvana nirvanaBand = new Nirvana();
+	ArrayList<Song> nirvanaTracks = new ArrayList<Song>();
+	nirvanaTracks = nirvanaBand.getNirvanaSongs();
+	//add tracks to playlist
+	playlist.add(nirvanaTracks.get(0));
+	playlist.add(nirvanaTracks.get(1));
+	playlist.add(nirvanaTracks.get(2));
 	
     return playlist;
 	}
