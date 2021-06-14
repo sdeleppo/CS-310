502a2d6f16e3fce6b545709ac3d19b5bd9e843e4
Tyson Smith <tysonsmiths@gmail.com>
2021-06-13 19:22:20 -0600

    Further fixes to spacing.

--- src/snhu/student/playlists/TysonSmith_Playlist.java
+++ src/snhu/student/playlists/TysonSmith_Playlist.java
@@ -9,35 +9,35 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class TysonSmith_Playlist {
-    
-	public LinkedList<PlayableSong> StudentPlaylist(){
-	
-	LinkedList<PlayableSong> playlist = new LinkedList<PlayableSong>();
-	
-	// Eninem Songs
-	ArrayList<Song> eminemTracks = new ArrayList<Song>();
-  Eminem eminem = new Eminem();
-  eminemTracks = eminem.getEminemSongs();
-	
-	playlist.add(eminemTracks.get(1)); // Rap God
-	
-	// Paulo Londra songs
-	ArrayList<Song> pauloLondraTracks = new ArrayList<Song>();
-  PauloLondra pauloLondra = new PauloLondra();
-  pauloLondraTracks = pauloLondra.getPauloLondraSongs();
-	
-	playlist.add(pauloLondraTracks.get(0)); // Forever Alone
-	playlist.add(pauloLondraTracks.get(1)); // Chica Paranormal
-	playlist.add(pauloLondraTracks.get(2)); // Solo Pienso En Ti
-	playlist.add(pauloLondraTracks.get(3)); // Querido Amigo
-	
-	// Eric Church Songs
-	ArrayList<Song> ericChurchTracks = new ArrayList<Song>();
-  EricChurch ericChurch = new EricChurch();
-  ericChurchTracks = ericChurch.getEricChurchSongs();
-	
-	playlist.add(ericChurchTracks.get(0)); // Heart on Fire
-	
-    return playlist;
-	}
+
+    public LinkedList < PlayableSong > StudentPlaylist() {
+
+        LinkedList < PlayableSong > playlist = new LinkedList < PlayableSong > ();
+
+        // Eninem Songs
+        ArrayList < Song > eminemTracks = new ArrayList < Song > ();
+        Eminem eminem = new Eminem();
+        eminemTracks = eminem.getEminemSongs();
+
+        playlist.add(eminemTracks.get(1)); // Rap God
+
+        // Paulo Londra songs
+        ArrayList < Song > pauloLondraTracks = new ArrayList < Song > ();
+        PauloLondra pauloLondra = new PauloLondra();
+        pauloLondraTracks = pauloLondra.getPauloLondraSongs();
+
+        playlist.add(pauloLondraTracks.get(0)); // Forever Alone
+        playlist.add(pauloLondraTracks.get(1)); // Chica Paranormal
+        playlist.add(pauloLondraTracks.get(2)); // Solo Pienso En Ti
+        playlist.add(pauloLondraTracks.get(3)); // Querido Amigo
+
+        // Eric Church Songs
+        ArrayList < Song > ericChurchTracks = new ArrayList < Song > ();
+        EricChurch ericChurch = new EricChurch();
+        ericChurchTracks = ericChurch.getEricChurchSongs();
+
+        playlist.add(ericChurchTracks.get(0)); // Heart on Fire
+
+        return playlist;
+    }
 }
