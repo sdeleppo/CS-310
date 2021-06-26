ebbe9bc767213d1f8387af1114ac913c153da55a
Kate Kowalyshyn <kate.kowalyshyn@snhu.edu>
2021-06-26 08:15:04 -0400

    Added songs to Morgan Wallen and Eric Church for code review.

--- src/music/artist/EricChurch.java
+++ src/music/artist/EricChurch.java
@@ -17,8 +17,10 @@ public class EricChurch {
     	 albumTracks = new ArrayList<Song>();                                   //Instantiate the album so we can populate it below
     	 Song track1 = new Song("Heart on Fire", "Eric Church");   				//Create a song
          Song track2 = new Song("Crazyland", "Eric Church");       			    //Create another song
+         Song track3 = new Song("Hell Of A View", "Eric Church");				//Create third song for Eric Church
          this.albumTracks.add(track1);                                          //Add the first song to song list for Eric Church
          this.albumTracks.add(track2);                                          //Add the second song to song list for Eric Church 
+         this.albumTracks.add(track3);											//Add the third song to song list for Eric Church
          return albumTracks;                                                    //Return the songs for Eric Church in the form of an ArrayList
     }
 }
--- src/music/artist/MorganWallen.java
+++ src/music/artist/MorganWallen.java
@@ -18,9 +18,11 @@ public class MorganWallen {
     	 Song track1 = new Song("Bandaid on a Bullet Hole", "Morgan Wallen");   //Create a song
          Song track2 = new Song("Silverado for Sale", "Morgan Wallen");         //Create another song
          Song track3 = new Song("Outlaw", "Morgan Wallen");						//Create another song
+         Song track4 = new Song("Somebody's Problem", "Morgan Wallen");			//Create another song
          this.albumTracks.add(track1);                                          //Add the first song to song list for Morgan Wallen
          this.albumTracks.add(track2);                                          //Add the second song to song list for Morgan Wallen 
          this.albumTracks.add(track3);											//Add the third song to song list for Morgan Wallen
+         this.albumTracks.add(track4);											//Add the fourth song to the song list for Morgan Wallen
          return albumTracks;                                                    //Return the songs for Morgan Wallen in the form of an ArrayList
     }
 }
